c05a02c763870e91d4a99090d53f86f1ab0533e2
masasaeki <masaakisaeki@gmail.com>
2022-12-31 11:48:08 +0900

    CSSカードコンポーネント

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,7 +1,7 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @inject('colorPresenter', '\App\Services\Presenters\ColorService')
 <style>
-.grid-auto-fit {
+.sites-container {
   display: grid;
   border: 1px solid #bbb;
   grid-gap: 20px;
@@ -20,6 +20,8 @@
   border: 1px solid #bbb;
   padding: 4px 16px;
   border-radius: 5px;
+  display: flex;
+  flex-direction: column;
 }
 .site__url {
   word-wrap: break-word;
@@ -28,6 +30,12 @@
   /* aspect-ratio: 125 / 87;
   object-fit: cover; */
   width: 100%;
+}
+.site__colors {
+  margin-top: auto;
+}
+.site .site__item:not(:last-child) {
+  border-bottom: 1px solid #bbb;
 }
 </style>
 <div>
@@ -51,37 +59,39 @@
     </select>
   </form>
 </div>
-<div class="grid-auto-fit">
+<div class="sites-container">
 @foreach ($sites as $site)
 <div class="site">
-  <p class="site__img">
+  <p class="site__item site__img">
     <img src="{{ asset("storage/images/$site->imgsrc") }}">
   </p>
-  <p class="site__url">
+  <p class="site__item site__url">
     {{ $site->url }}<br>
     ({{ $site->domain }})
   </p>
-  <p>
+  <p class="site__item site__title">
     {{ $site->title }}
   </p>
-  <p>
+  <p class="site__item site__description">
     {{ $site->description }}
   </p>
-  <p @class(['color', 'text-white' => $colorPresenter->isTextColorWhite($site->mode_color)]) style="border: 1px solid #333; width: 100px; height: 100px; background-color: #{{ $site->mode_color }};">
-    {{ $site->mode_color }}
-  </p>
-  <p @class(['color', 'text-white' => $colorPresenter->isTextColorWhite($site->second_color)]) style="border: 1px solid #333; width: 100px; height: 100px; background-color: #{{ $site->second_color }};">
-    {{ $site->second_color }}
-  </p>
-  <p @class(['color', 'text-white' => $colorPresenter->isTextColorWhite($site->third_color)]) style="border: 1px solid #333; width: 100px; height: 100px; background-color: #{{ $site->third_color }};">
-    {{ $site->third_color }}
-  </p>
-  <p @class(['color', 'text-white' => $colorPresenter->isTextColorWhite($site->darkest_color)]) style="border: 1px solid #333; width: 100px; height: 100px; background-color: #{{ $site->darkest_color }};">
-    {{ $site->darkest_color }}
-  </p>
-  <p @class(['color', 'text-white' => $colorPresenter->isTextColorWhite($site->brightest_color)]) style="border: 1px solid #333; width: 100px; height: 100px; background-color: #{{ $site->brightest_color }};">
-    {{ $site->brightest_color }}
-  </p>
+  <div class="site__item site__colors">
+    <p @class(['color', 'text-white' => $colorPresenter->isTextColorWhite($site->mode_color)]) style="border: 1px solid #333; width: 100px; height: 100px; background-color: #{{ $site->mode_color }};">
+      {{ $site->mode_color }}
+    </p>
+    <p @class(['color', 'text-white' => $colorPresenter->isTextColorWhite($site->second_color)]) style="border: 1px solid #333; width: 100px; height: 100px; background-color: #{{ $site->second_color }};">
+      {{ $site->second_color }}
+    </p>
+    <p @class(['color', 'text-white' => $colorPresenter->isTextColorWhite($site->third_color)]) style="border: 1px solid #333; width: 100px; height: 100px; background-color: #{{ $site->third_color }};">
+      {{ $site->third_color }}
+    </p>
+    <p @class(['color', 'text-white' => $colorPresenter->isTextColorWhite($site->darkest_color)]) style="border: 1px solid #333; width: 100px; height: 100px; background-color: #{{ $site->darkest_color }};">
+      {{ $site->darkest_color }}
+    </p>
+    <p @class(['color', 'text-white' => $colorPresenter->isTextColorWhite($site->brightest_color)]) style="border: 1px solid #333; width: 100px; height: 100px; background-color: #{{ $site->brightest_color }};">
+      {{ $site->brightest_color }}
+    </p>
+  </div>
 </div>
 @endforeach
 </div>
