cb29ef73d6d155d24ce54c172b2d6a0f560c4b0b
masasaeki <masaakisaeki@gmail.com>
2023-01-05 13:06:35 +0900

    リンク範囲が広すぎるので調整

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -11,14 +11,14 @@
   @endif
 </div>
 <div class="inputbox colorbox">
-  <div>
+  <div class="flex items-center">
     <a class="flex" href="{{ route('sites.index') }}">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
       </svg>
       ホーム</a>
   </div>
-  <div>
+  <div class="flex items-center">
     <a class="flex" href="{{ route('sites.create') }}">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
