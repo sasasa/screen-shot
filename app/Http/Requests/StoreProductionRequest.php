c418e67039438386cb4bf35c1db3c05b03c5bb14
masasaeki <masaakisaeki@gmail.com>
2023-01-14 15:04:16 +0900

    Web制作会社のバリデーションをもっとしっかり作る　#40

--- app/Http/Requests/StoreProductionRequest.php
+++ app/Http/Requests/StoreProductionRequest.php
@@ -26,16 +26,33 @@ class StoreProductionRequest extends FormRequest
         return [
             'company_type' => 'required|integer|min:1|max:3',
             'name' => 'required|string|max:255',
-            'kana' => 'required|string|max:255',
+            // カタカナだけのバリデーション
+            'kana' => 'required|string|max:255|regex:/^[ァ-ヾ]+$/u',
             'representative' => 'required|string|max:255',
             'inquiry_email' => 'required|string|email:strict,dns,spoof|max:255|unique:productions,inquiry_email,'. $this->user()->id . ',id',
-            'postal' => 'required|string|max:255',
+            // 郵便番号のバリデーション
+            'postal' => 'required|string|regex:/^\d{3}-\d{4}$/',
             'address' => 'required|string|max:255',
-            'phone' => 'required|string|max:255',
-            'url' => 'required|string|max:255',
+            // 電話番号のバリデーション
+            'phone' => 'required|string|regex:/^(0{1}\d{1,4}-{0,1}\d{1,4}-{0,1}\d{4})$/',
+            'url' => 'required|string|max:255|url',
             'staff' => 'required|integer|min:1|max:7',
             'achievement' => 'required|string|max:5000',
             'introduction' => 'required|string|max:5000',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages()
+    {
+        return [
+            'postal.regex' => '郵便番号は正しい形式で入力してください',
+            'kana.regex' => 'カタカナで入力してください',
+            'phone.regex' => '電話番号は正しい形式で入力してください',
         ];
     }
 }
